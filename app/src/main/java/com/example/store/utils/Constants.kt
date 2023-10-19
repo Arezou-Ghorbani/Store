7fb990273bb3a9ca0386b96251fccd1d96fa2ca8
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-10-19 12:42:16 +0330

    SessionManager.kt created

---
+++ app/src/main/java/com/example/store/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.store.utils
+
+/**Created by Arezou-Ghorbani on 19,October,2023,ArezouGhorbaniii@gmail.com**/
+//Session
+const val SESSION_AUTH_DATA = "session_auth_data"
+const val USER_TOKEN_DATA = "user_token_data"
