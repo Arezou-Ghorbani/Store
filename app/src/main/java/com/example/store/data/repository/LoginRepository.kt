cd49ad1f213b1d34ffd20bbf9912aaf8117630a2
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-10-26 13:01:42 +0330

    LoginRepository.kt created

---
+++ app/src/main/java/com/example/store/data/repository/LoginRepository.kt
@@ -0,0 +1,10 @@
+package com.example.store.data.repository
+
+import com.example.store.data.models.login.BodyLogin
+import com.example.store.data.network.ApiServices
+import javax.inject.Inject
+
+/**Created by Arezou-Ghorbani on 26,October,2023,ArezouGhorbaniii@gmail.com**/
+class LoginRepository @Inject constructor(private val api: ApiServices) {
+        suspend fun postLogin(body: BodyLogin) = api.postLogin(body)
+    }
