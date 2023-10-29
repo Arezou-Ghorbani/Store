6f7186e2db6b000afd4ad66fca11a78da2a7bd7d
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-10-29 13:06:53 +0330

    LoginRepository.kt added

--- app/src/main/java/com/example/store/data/repository/LoginRepository.kt
+++ app/src/main/java/com/example/store/data/repository/LoginRepository.kt
@@ -6,5 +6,7 @@ import javax.inject.Inject
 
 /**Created by Arezou-Ghorbani on 26,October,2023,ArezouGhorbaniii@gmail.com**/
 class LoginRepository @Inject constructor(private val api: ApiServices) {
-        suspend fun postLogin(body: BodyLogin) = api.postLogin(body)
-    }
+    suspend fun postLogin(body: BodyLogin) = api.postLogin(body)
+    suspend fun postVerify(body: BodyLogin) = api.postVerify(body)
+
+}
--- app/src/main/java/com/example/store/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/store/viewModel/LoginViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.store.data.models.login.BodyLogin
 import com.example.store.data.models.login.ResponseLogin
+import com.example.store.data.models.login.ResponseVerify
 import com.example.store.data.repository.LoginRepository
 import com.example.store.utils.network.NetworkRequest
 import com.example.store.utils.network.NetworkResponse
@@ -24,5 +25,15 @@ class LoginViewModel @Inject constructor(private val repository: LoginRepository
         _loginData.value = NetworkRequest.Loading()
         val response = repository.postLogin(body)
         _loginData.value = NetworkResponse(response).generateResponse()
+    }
+
+    //Verify
+    private val _verifyData = MutableLiveData<NetworkRequest<ResponseVerify>>()
+    val verifyData: LiveData<NetworkRequest<ResponseVerify>> = _verifyData
+
+    fun callVerifyApi(body: BodyLogin) = viewModelScope.launch {
+        _verifyData.value = NetworkRequest.Loading()
+        val response = repository.postVerify(body)
+        _verifyData.value = NetworkResponse(response).generateResponse()
     }
 }
