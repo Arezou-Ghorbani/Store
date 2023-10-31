7492f3890687371acdc4e3cc6563c09425148450
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-10-31 14:41:00 +0330

    HomeFragment.kt added

---
+++ app/src/main/java/com/example/store/ui/cart/CartFragment.kt
@@ -0,0 +1,9 @@
+package com.example.store.ui.cart
+
+import com.example.store.utils.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+/**Created by Arezou-Ghorbani on 28,October,2023,ArezouGhorbaniii@gmail.com**/
+@AndroidEntryPoint
+class CartFragment : BaseFragment() {
+}
