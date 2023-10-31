4ce17c748999e777f5942c574e4ae8352daddcb9
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-10-31 14:42:54 +0330

    ProfileFragment.kt added

---
+++ app/src/main/java/com/example/store/ui/profile/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.example.store.ui.profile
+
+import com.example.store.utils.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+/**Created by Arezou-Ghorbani on 31,October,2023,ArezouGhorbaniii@gmail.com**/
+@AndroidEntryPoint
+class ProfileFragment : BaseFragment() {
+}
