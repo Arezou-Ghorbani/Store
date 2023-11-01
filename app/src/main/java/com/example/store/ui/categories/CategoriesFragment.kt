ace6f3e1c62844ffaf08b16e57ec81d71e0cb5a0
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-11-01 14:13:25 +0330

    CategoriesFragment and BaseDiffUtils added to project

---
+++ app/src/main/java/com/example/store/ui/categories/CategoriesFragment.kt
@@ -0,0 +1,11 @@
+package com.example.store.ui.categories
+
+import com.example.store.utils.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+/**Created by Arezou-Ghorbani on 29,October,2023,ArezouGhorbaniii@gmail.com**/
+
+@AndroidEntryPoint
+class CategoriesFragment : BaseFragment() {
+
+}
