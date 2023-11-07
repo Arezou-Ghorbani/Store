c2b149a1daaa9291ab3062cd6f3b80518db032fc
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-11-07 12:40:19 +0330

    categories models added to project

---
+++ app/src/main/java/com/example/store/ui/categories_products/CategoriesProductsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.store.ui.categories_products
+
+import com.example.store.utils.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+/**Created by Arezou-Ghorbani on 05,November,2023,ArezouGhorbaniii@gmail**/
+@AndroidEntryPoint
+class CategoriesProductsFragment : BaseFragment() {}
