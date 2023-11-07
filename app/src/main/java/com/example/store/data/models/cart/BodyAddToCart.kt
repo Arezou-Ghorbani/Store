900524b0ee0beb0b6c21714f16f8c218e1cacf36
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-11-07 14:26:02 +0330

    cart and comments model added

--- app/src/main/java/com/example/store/data/models/cart/BodyAddToCart.kt
+++ app/src/main/java/com/example/store/data/models/cart/BodyAddToCart.kt
@@ -2,7 +2,6 @@ package com.example.store.data.models.cart
 
 import com.google.gson.annotations.SerializedName
 
-/**Created by Arezou-Ghorbani on 28,October,2023,ArezouGhorbaniii@gmail.com**/
 data class BodyAddToCart(
     @SerializedName("colorId")
     var colorId: String? = null // 2
--- app/src/main/java/com/example/store/data/models/cart/ResponseCartList.kt
+++ app/src/main/java/com/example/store/data/models/cart/ResponseCartList.kt
@@ -2,8 +2,6 @@ package com.example.store.data.models.cart
 
 import com.google.gson.annotations.SerializedName
 
-/**Created by Arezou-Ghorbani on 28,October,2023,ArezouGhorbaniii@gmail.com**/
-
 data class ResponseCartList(
     @SerializedName("delivery_price")
     val deliveryPrice: String?, // 0
