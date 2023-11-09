e116bb1f17f510ae2cf218928c1e36c2295d3c5a
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-11-09 15:19:17 +0330

    CartRepository.kt and CartViewModel compeleted

--- app/src/main/java/com/example/store/data/repository/CartRepository.kt
+++ app/src/main/java/com/example/store/data/repository/CartRepository.kt
@@ -8,6 +8,10 @@ import javax.inject.Inject
 class CartRepository @Inject constructor(private val api: ApiServices) {
     suspend fun postAddToCart(id: Int, body: BodyAddToCart) = api.postAddToCart(id, body)
     suspend fun getCartList() = api.getCartList()
+    suspend fun putIncrementCart(id: Int) = api.putIncrementCart(id)
+    suspend fun putDecrementCart(id: Int) = api.putDecrementCart(id)
+    suspend fun deleteProductFromCart(id: Int) = api.deleteProductFromCart(id)
+    suspend fun getCartContinueList() = api.getCartContinueList()
 
 
 }
